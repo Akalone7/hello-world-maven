c61853af639c5a53032455a04f17d7b3df51e61a
Clelia <clelia_devietro@hotmail.it>
2016-02-21 22:09:24 +0100

    Primo metodo rest get

--- src/main/java/it/clefra/web/controllers/MasterController.java
+++ src/main/java/it/clefra/web/controllers/MasterController.java
@@ -29,14 +29,14 @@ public class MasterController {
 	private static final Logger LOGGER = LoggerFactory.getLogger( MasterController.class );
 
 	public static final String API_ROOT_URI = "/master";
-	
+
 	//TODO inserire in un layer di servizi
 	@Autowired
 	private DummyRepository dummyRepository;
 
-//	Da utilizzare perl'internazionalizzazione TODO
-//	@Autowired
-//	private MessageSource messageSource;
+	//	Da utilizzare perl'internazionalizzazione TODO
+	//	@Autowired
+	//	private MessageSource messageSource;
 
 	/**
 	 * @return
@@ -46,14 +46,14 @@ public class MasterController {
 		LOGGER.debug("Start getting dummy elements");
 		List<DummyDto> output = new ArrayList<>();
 		DummyDto dummyDto;
-		
+
 		List<DummyModel> allDummy = dummyRepository.findAll();
-		
+
 		//TODO Web-Persistence Mapper
 		for(DummyModel model : allDummy){
 			dummyDto = new DummyDto();
 			dummyDto.setId(model.getId() != null ? model.getId().toHexString() : null)
-					.setValue(model.getValue());
+			.setValue(model.getValue());
 			output.add(dummyDto);
 		}
 		LOGGER.debug("{} dummy found -> {};", allDummy.size(), allDummy);
@@ -61,7 +61,26 @@ public class MasterController {
 		LOGGER.debug("Request computed. Returning response to Client");
 		return response;
 	}
-	
+
+
+
+	/**
+	 * @return
+	 */
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<DummyDto> get(@PathVariable String id) {
+		LOGGER.debug("Start getting dummy elements");
+		ObjectId objectId = new ObjectId(id);
+		DummyDto dummyDto = new DummyDto();
+		DummyModel dummyModel = dummyRepository.findOne(objectId);
+		dummyDto.setId(dummyModel.getId() != null? dummyModel.getId().toHexString() : null) 
+		.setValue(dummyModel.getValue());
+		
+		ResponseEntity<DummyDto> response = new  ResponseEntity<DummyDto>(dummyDto, HttpStatus.OK);
+		LOGGER.debug("Request computed. Returning response to Client");
+		return response;
+	}
+
 	/**
 	 * @return
 	 */
@@ -70,14 +89,14 @@ public class MasterController {
 		LOGGER.debug("Start insert dummy element");
 		DummyModel dummy = new DummyModel();
 		DummyDto output = null;
-		
+
 		//TODO Web<->Persistence Mapper
 		dummy.setValue(dummyDto.getValue());
 		try{
-		DummyModel insertedDummy = dummyRepository.insert(dummy);
-		output = new DummyDto()
-					.setId(insertedDummy.getId() != null ? insertedDummy.getId().toHexString() : null)
-					.setValue(insertedDummy.getValue());
+			DummyModel insertedDummy = dummyRepository.insert(dummy);
+			output = new DummyDto()
+			.setId(insertedDummy.getId() != null ? insertedDummy.getId().toHexString() : null)
+			.setValue(insertedDummy.getValue());
 		} catch (final Exception e){
 			LOGGER.error(e.getMessage(), e);
 		}
@@ -85,16 +104,16 @@ public class MasterController {
 		LOGGER.debug("Request computed. Returning response to Client");
 		return response;
 	}
-	
+
 	/**
 	 * @return
 	 */
 	@RequestMapping(value= "/{dummyId}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String dummyId) {
 		LOGGER.debug("Start deleting dummy with id {};", dummyId);
-		
+
 		ResponseEntity<Void> response = new ResponseEntity<Void>(HttpStatus.FORBIDDEN);
-		
+
 		//TODO Web-Persistence Mapper
 		try{
 			dummyRepository.delete(new ObjectId(dummyId));
@@ -106,5 +125,5 @@ public class MasterController {
 		LOGGER.debug("Request computed. Returning response to Client");
 		return response;
 	}
-	
+
 }
