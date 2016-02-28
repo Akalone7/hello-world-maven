2032a4ce8e08b3ba6d392501820c47bbf0e7bbe4
Clelia <clelia_devietro@hotmail.it>
2016-02-28 17:44:43 +0100

    Jwt token

---
+++ src/main/java/it/clefra/spring/security/model/JwtAuthenticationToken.java
@@ -0,0 +1,14 @@
+package it.clefra.spring.security.model;
+
+public class JwtAuthenticationToken {
+	private String token;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+}
