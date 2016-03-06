b6c62342214764e7cf95474001294b9bdbb72cf0
Clelia <clelia_devietro@hotmail.it>
2016-03-06 20:26:52 +0100

    fix user detail

--- src/main/java/it/clefra/web/controllers/UserController.java
+++ src/main/java/it/clefra/web/controllers/UserController.java
@@ -3,6 +3,7 @@ package it.clefra.web.controllers;
 import it.clefra.persistence.model.CredentialsModel;
 import it.clefra.persistence.model.DummyModel;
 import it.clefra.persistence.repositories.CredentialsRepository;
+import it.clefra.spring.security.InvalidJwtAuthenticationException;
 import it.clefra.spring.security.JwtTokenGenerator;
 import it.clefra.spring.security.JwtUtil;
 import it.clefra.spring.security.model.JwtUser;
@@ -20,25 +21,35 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @RepositoryRestController @RequestMapping (UserController.API_ROOT_URI)	
 public class UserController {
-	
+
 	public static final String API_ROOT_URI = "/user";
 	private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+	public static final String AUTHORIZATION_HEADER_NAME = "Authorization";
+	public static final String JWT_HEADER_PREFIX = "Bearer";
+	public static final Integer JWT_HEADER_LENGTH = 7;
+
 	@Autowired
 	private CredentialsRepository credentialRepository;
 	@Autowired
 	private JwtUtil jwtUtil;
 
-	@RequestMapping(value = "/{jwt}", method = RequestMethod.GET)
-	public ResponseEntity<UserDetailDto> get(@PathVariable String jwt) {
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<UserDetailDto> get(@RequestHeader(AUTHORIZATION_HEADER_NAME) String authorizationHeader) {
 		LOGGER.debug("Start getting user");
 		String username = null;
 		//determino l'user a partire dal jwt
-		JwtUser jwtUser = jwtUtil.parseToken(jwt); 
+		if (authorizationHeader == null || !authorizationHeader.startsWith(JWT_HEADER_PREFIX)) {
+			throw new InvalidJwtAuthenticationException("No JWT token found in request headers"); //TODO Custom Exception.
+		}
+		String jwtToken = authorizationHeader.substring(JWT_HEADER_LENGTH);
+
+		JwtUser jwtUser = jwtUtil.parseToken(jwtToken); 
 		if(jwtUser != null) {
 			username = jwtUser.getUsername();
 		}
@@ -47,8 +58,6 @@ public class UserController {
 		if(crendentialModel != null) {
 			userDetailDto.setUsername(username);
 		}
-		
-		
 		ResponseEntity<UserDetailDto> response = new  ResponseEntity<UserDetailDto>(userDetailDto, HttpStatus.OK);
 		LOGGER.debug("Request computed. Returning response to Client");
 		return response;
